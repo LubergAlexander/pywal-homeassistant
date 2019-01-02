07bf5ca14685346ba6a70da2840c3562dbaa989f
Alexander Luberg <alex@luberg.me>
2019-01-01 20:22:52 -0800

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# pywal-homeassistant
+
+## Intro
+Sets lighting colors based on wallpaper color palette
+
+## Usage
+
+Set homeassistant `BASE_URL` and `PASSWORD` in `pywal-homeassistant.py`
+
+Map colors to various light enitites in HA
+
+```
+pip install -r requirements.txt
+python /path/to/pywal-homeassistant.py
+```
+
+or can also be chained to pywal call
+
+`wal -i image.jpg -o /path/to/pywal_homeassistant.py`
---
+++ pywal-homeassistant.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+
+BASE_URL = "http://homeassistant:8123"
+PASSWORD = "CHANGEME"
+WAL_CACHE_FILE = "/home/alex/.cache/wal/colors.json"
+
+
+def hex_to_rgb(value):
+    value = value.lstrip("#")
+    lv = len(value)
+
+    return tuple(int(value[i : i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+
+def call_service(domain, service, data):
+    return requests.post(
+        "{base_url}/api/services/{domain}/{service}".format(base_url=base_url),
+        json=data,
+        headers={"X-HA-Access": PASSWORD},
+    )
+
+
+def set_light_color(entity_id, color, brightness_pct=60):
+    return call_service(
+        "light",
+        "turn_on",
+        {"entity_id": entity_id, "brightness_pct": brightness_pct, "rgb_color": color},
+    )
+
+
+def main():
+    with open(WAL_CACHE_FILE, "r") as theme_file:
+        theme = json.loads(theme_file.read())
+
+    primary = hex_to_rgb(theme["colors"]["color3"])
+    secondary = hex_to_rgb(theme["colors"]["color1"])
+
+    set_light_color("light.officedesk", primary)
+    set_light_color("light.aurora", secondary)
+
+
+if __name__ == "__main__":
+    main()
